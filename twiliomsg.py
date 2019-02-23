15ed58ab1ccd7cfca75d540d7fa8e40a33f988eb
azimshaik <cheer.buddu@gmail.com>
2019-02-23 13:54:20 -0500

    twiliomsg

---
+++ twiliomsg.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client 
+ 
+account_sid = 'AC7c35d6d25a694ba957e5243c9c5be4f0' 
+auth_token = '' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create( 
+                              from_='+19193240870',        
+                              to='+19198892549' ,
+                              body='Alert!!!!!!'
+                          ) 
+ 
+print(message.sid)
